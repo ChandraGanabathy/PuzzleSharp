dc52799758b5cadeda0846fad6063b9449bff7a6
Chandra Ganabathy <ganabathy@PLSProducts.net>
2024-06-05 12:15:44 -0400

    adding test

---
+++ PuzzleSharp.Test/OccurenceTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PuzzleSharp.UnitTest
+{
+    [TestClass]
+    public class OccurenceTest
+    {
+        [TestMethod]
+        public void AreEqualtotheInputkeyvalue()
+        {
+            char[] input = "aaabbcdffffffffffffhhhhhhhh".ToCharArray();
+            Occurence _occurence = new Occurence(input);
+            _occurence.CountOccurenceorderoflogN(0, input.Length - 1);
+            Assert.AreEqual(_occurence.dict['f'], 12);
+        }
+        public void AreEqualtotheInputkeyvalueorderofN()
+        {
+            char[] input = "aaabbcdffffffffffffhhhhhhhh".ToCharArray();
+            Occurence _occurence = new Occurence(input);
+            _occurence.CountOccurenceOrderofN(0, input.Length - 1);
+            Assert.AreEqual(_occurence.dict['f'], 12);
+        }
+        public void AreEqualtotheInputkeyvalue_large()
+        {
+            char[] input = "aaabbcdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy".ToCharArray();
+            Occurence _occurence = new Occurence(input);
+            _occurence.CountOccurenceorderoflogN(0, input.Length - 1);
+            Assert.AreEqual(_occurence.dict['a'], 3);
+        }
+        public void AreEqualtotheInputkeyvalueorderofN_large()
+        {
+            char[] input = "aaabbcdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz".ToCharArray();
+            Occurence _occurence = new Occurence(input);
+            _occurence.CountOccurenceOrderofN(0, input.Length - 1);
+            Assert.AreEqual(_occurence.dict['a'], 3);
+        }
+    }
+}
